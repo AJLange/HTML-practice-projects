024be3bce8edb90faa0a8a03cbc8fcdd648c254a
Amanda Lange <litagemini@gmail.com>
2021-12-11 00:46:21 -0500

    Assignment 3

---
+++ assign3/wk1.js
@@ -0,0 +1,16 @@
+function greet()
+{
+var myName = "Amanda";
+//var myName = "Steve";
+var greeting= "Hello";
+if (myName === "Steve")
+    {
+console.log(`Steve here AKA the Stevinator, just wanted to say ${greeting}`);
+    }
+    else
+    {
+        console.log(`${greeting}, my name is ${myName}`);
+    }
+}
+
+greet();
